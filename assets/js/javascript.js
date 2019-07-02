f269a435c70078d4fb79e2fe44c3d4489eb9c24b
acarlie <ameliacarlie@gmail.com>
2019-07-02 10:46:38 -0400

    Created jQuery Scrollspy

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,27 @@
-//Basic fade in to cover up delayed loading of webfonts
 $(document).ready(function(){
     $('#fade-wrapper').fadeIn(1000);
+
+    var scrollMenuIds = $('a.nav-link[href]');
+
+    $(document).scroll(function(){
+        scrollMenuIds.each(function(){
+
+            var container = $(this).attr('href');
+            var containerOffset = $(container).offset().top;
+            var containerHeight = $(container).outerHeight();
+            var containerBottom = containerOffset + containerHeight;
+            var scrollPosition = $(document).scrollTop();
+    
+            if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
+                $(this).addClass('active');
+            } else{
+                $(this).removeClass('active');
+            }
+    
+    
+        });
+    });
+   
+
+
 });
