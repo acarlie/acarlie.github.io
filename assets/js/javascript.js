e1ac94b4f3792b179ffab01527beda0de953df81
acarlie <ameliacarlie@gmail.com>
2019-07-31 22:27:19 -0400

    Created util object and methods

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,6 @@
 var port = {
     portfolio: document.getElementById('portfolio'),
     items:[
-        // {title: "title", img: "http://", desc: "", tags: "", url: ""},
         {title: "Color Me ____.", img: "assets/images/portfolio/color-me-5.JPG", desc: "Word game created with Vanilla JS", tags: ["Vanilla JS", "UI/UX"], url: "https://acarlie.github.io/Word_Guess_Game/"},
         {title: "Trivia Game", img: "assets/images/portfolio/trivia-game.JPG", desc: "Timed Trivia Game created using jQuery and JavaScript", tags: ["jQuery, JavaScript"], url: "https://acarlie.github.io/Trivia_Game/"},
         {title: "A Stranger Game", img: "assets/images/portfolio/a-stranger-game-2.JPG", desc: "Player vs algorithm game created using jQuery and JavaScript", tags: ["jQuery"], url: "https://acarlie.github.io/Stranger_Things_Game/"},
@@ -42,28 +41,27 @@ var typewriter = {
         this.word = this.textArray[this.counter];
     },
     deleteLetters(){
-        if (this.letter > 0 && !this.add){ //delete until letter count = 0
+        if (this.letter > 0 && !this.add){ 
             this.letter--;
             var textContent = this.el.textContent;
             this.el.textContent = textContent.substring(0, this.letter); 
-        } else if (this.letter === 0 && !this.add){ //when letter count = 0, start adding next word
+        } else if (this.letter === 0 && !this.add){ 
             this.add = true;
             this.el.innerHTML = '';
-            this.counter++; //next word
+            this.counter++; 
             this.setWord();
             this.startAdd();
         }
     },
     addLetters() {
-        //if all words have been looped through
         if (this.counter === this.textArray.length) {
             this.type();
-        } else { //otherwise start word
+        } else { 
             this.setWord();
-            if (this.letter < this.word.length && this.add) { //before word is complete keep adding
+            if (this.letter < this.word.length && this.add) { 
                 this.el.textContent += this.word[this.letter];
                 this.letter++;
-            } else if (this.letter === this.word.length && this.add) { //when letter is complete start deleting
+            } else if (this.letter === this.word.length && this.add) { 
                 this.add = false;
                 document.getElementById('blinker').classList = "blink";
                 setTimeout(function(){ typewriter.startDelete(); }, 1500);
@@ -81,57 +79,55 @@ var typewriter = {
     }
 }
 
-var utilities = {
-    menuFadeIn(){
-
+var util = {
+    init(){
+        $('#fade-wrapper').fadeIn(1000);
+        $('#nav-wrapper').hide();
     },
-    scrollSpy(){
+    mobileMenu(){
+        $('#nav').toggleClass('nav-visible');
+    },
+    menuFade(scrollTop, secondChild){
+        if (scrollTop < secondChild){
+            $('#nav-wrapper').fadeOut(500);
+        } else if (scrollTop > secondChild){
+            $('#nav-wrapper').fadeIn(500);
 
+        }
     }
 }
 
 
 $(document).ready(function(){
-
-    $('#fade-wrapper').fadeIn(1000);
-
-    typewriter.type();
-
-    port.generate();
+    util.init();
 
     var scrollMenuIds = $('a.nav-link[href]');
-
-    $("#menu").click(function() {
-        $('#nav').toggleClass('nav-visible');
-    });
-
-    $('#nav-wrapper').hide();
-
     var secondChildPos = $('main section:nth-child(2)').offset().top * .75;
+
+    typewriter.type();
+    port.generate();
+
+    //mobile menu
+    $("#menu").click(util.mobileMenu);
+
 
     $(document).scroll(function(){
         var scrollPosition = $(document).scrollTop();
-      
-        if (scrollPosition < secondChildPos){
-            $('#nav-wrapper').fadeOut(500);
-        } else if (scrollPosition > secondChildPos){
-            $('#nav-wrapper').fadeIn(500);
+        
+        util.menuFade(scrollPosition, secondChildPos);
 
-        }
-
-        scrollMenuIds.each(function(){
-
-            var container = $(this).attr('href');
+        scrollMenuIds.each(function(i){
+            var link = scrollMenuIds[i];
+            var container = $(link).attr('href');
             var containerOffset = $(container).offset().top;
             var containerHeight = $(container).outerHeight();
             var containerBottom = containerOffset + containerHeight;
     
             if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
-                $(this).addClass('active');
+                $(link).addClass('active');
             } else{
-                $(this).removeClass('active');
+                $(link).removeClass('active');
             }
-    
     
         });
     });
