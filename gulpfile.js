88517d2b803fda0f3bd8b964585bb5fe15c8394b
acarlie <ameliacarlie@gmail.com>
2019-11-07 16:26:09 -0500

    Added image resizing to Gulp, added portfolio item

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean-css');
+const rename = require('gulp-rename');
+const imagemin = require('gulp-imagemin');
+// var Jimp = require('jimp');
+const jimp = require('gulp-jimp');
 
-let cleanCSS = require('gulp-clean-css');
 
-var uglify = require('gulp-uglify');
-var pipeline = require('readable-stream').pipeline;
- 
-
-gulp.task('message', async function() {
-    console.log("Good News Everyone!");
-});
- 
-gulp.task('prefix', () =>
-    gulp.src('assets/css/main.css')
+gulp.task('css', function () {
+    return gulp.src('assets/sass/main.scss')
+        .pipe(sass())
         .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('assets/dist'))
-);
-
-gulp.task('minify-css', () => {
-    return gulp.src('assets/dist/*.css')
-      .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('assets/dist/min'));
+        .pipe(clean())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/dist'));
 });
 
- 
-gulp.task('compress', function () {
-  return pipeline(
-        gulp.src('assets/js/*.js'),
-        uglify(),
-        gulp.dest('assets/dist')
-  );
+gulp.task('img-res', function () {
+    return gulp.src('assets/images/portfolio/*').pipe(jimp({
+        '-sm': {
+            resize: { width: 1300, height: jimp.Auto }
+        }
+    })).pipe(gulp.dest('assets/dist/images/sm'));
 });
-  
+
+gulp.task('img-min', function(){
+    return gulp.src('assets/images/portfolio/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('assets/dist/images'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/sass/*.scss', gulp.series('css'));
+});
