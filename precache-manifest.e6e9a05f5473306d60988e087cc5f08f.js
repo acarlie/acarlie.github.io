7560a8759c17428424cdeba1e934906823c71713
acarlie <ameliacarlie@gmail.com>
2019-12-20 18:25:07 -0500

    Updates

---
+++ precache-manifest.e6e9a05f5473306d60988e087cc5f08f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0cad90dc09ed78f640bac912598c0c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fa34358600d0e5c9eeb",
+    "url": "/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "05467af967334ee8dd72",
+    "url": "/static/css/main.7e9467d5.chunk.css"
+  },
+  {
+    "revision": "6fa34358600d0e5c9eeb",
+    "url": "/static/js/2.57160ef1.chunk.js"
+  },
+  {
+    "revision": "c65614ba1b7c3b5134a1ed89f56b30da",
+    "url": "/static/js/2.57160ef1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05467af967334ee8dd72",
+    "url": "/static/js/main.62c143bf.chunk.js"
+  },
+  {
+    "revision": "a941e102d80c0e1c5984",
+    "url": "/static/js/runtime-main.3c4bf554.js"
+  },
+  {
+    "revision": "d5a59ed9304e8d327bba63f78c78c348",
+    "url": "/static/media/profile_square.d5a59ed9.png"
+  }
+]);
--- src/components/Button/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button (props) {
-    return (
-        <button className='btn' onClick={() => { props.handler(props.title) }}>{props.title}</button>
-    );
-}
-
-export default Button;
