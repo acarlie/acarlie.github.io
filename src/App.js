7ef8567a25ed599fe4e67cf260a6e69f94dd729e
acarlie <ameliacarlie@gmail.com>
2021-01-12 21:40:12 -0500

    wip(styles): adding new styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 
 // packages
-import Trianglify from 'trianglify';
 import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
-import ScrollAnimation from 'react-animate-on-scroll';
 
 //components
-import TypeWriter from './components/TypeWriter';
+// import TypeWriter from './components/TypeWriter';
 import SocialLinks from './components/SocialLinks';
-import IconContainer from './components/IconContainer';
+// import IconContainer from './components/IconContainer';
 import PortfolioItem from './components/PortfolioItem';
-
+import makeAnimated from 'react-select/animated';
 //assets
-import Profile from './assets/images/profile_square.png';
+// import Profile from './assets/images/profile_square.png';
 import items from './assets/js/portfolioItems';
 
-//css
-import "animate.css/animate.min.css";
 
 const animatedComponents = makeAnimated();
 const menuStyles = {
@@ -42,8 +37,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setBg();
-    window.addEventListener("resize", this.setBg.bind(this));
+
     setTimeout(() => {
       this.setState({
         loader: 'done',
@@ -57,16 +51,7 @@ class App extends Component {
     }, 4000)
   }
 
-  setBg = () => {
-    let pattern = Trianglify({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      cell_size: 80,
-      variance: "0.89",
-      x_colors: ["#FFA9DC", "#F3CAFF", "#EB6087", "#E0004D", "#8F1368"]
-    });
-    pattern.canvas(document.getElementById('canvas-basic'));
-  }
+
 
   filterHandler = (input) => {
     const filterArr = input || false;
@@ -95,123 +80,42 @@ class App extends Component {
     return (
       <div className="App">
 
-        {/* Loader */}
-        {
-          !this.state.loaded &&
-          <div id="loader" className={"container-dark " + this.state.loader}>
-            <div id="loader-inner">
-              <h5>Loading</h5>
-              <div id="loader-expand"></div>
-            </div>
-          </div>
-        }
+
 
         {/* Content */}
         <div id="fade-wrapper">
           <main>
 
-            {/* Background */}
-            <div className="container" id="bg">
-              <canvas id="canvas-basic"></canvas>
-            </div>
 
             {/* Home */}
-            <section className="container" id="home">
+            <section className="g-container g-container--dark" id="home">
               <div className="wrapper">
                 <div>
-                  <div id="type-wrap">
-                    <div id="type-wrap-inner" className="box">
-                      <TypeWriter period={200} textArray={["Amelia Carlie", "Development", "& Design"]} />
-                      <hr className="home" />
-                      <nav className="text-center nav-main">
-                        <a href="#about">About</a>
-                        <a href="#work">Work</a>
-                        <a href="#about">Contact</a>
-                      </nav>
-                    </div>
-
-                    <a href={process.env.PUBLIC_URL + '/assets/pdfs/Amelia_Carlie_Resume.pdf'} target="_blank" rel="noopener noreferrer" className="btn-dark">Download My Resume</a>
-
-                  </div>
-
-                  <SocialLinks />
+                  Hello
                 </div>
               </div>
             </section>
 
             {/* About */}
-            <section className="container container-dark margin-bottom" id="about">
-
-              <div className="wrapper-nav">
-                <span className="fas fa-chevron-up"></span>
-                <a href="#home" className="wrapper-link">Home</a>
-              </div>
+            <section className="g-container g-container--light" id="about">
 
               <div className="wrapper">
                 <div className="row">
 
-                  <div className="col col-2">
-                    <ScrollAnimation animateIn="fadeIn" animateOut='fadeOut' offset={0} duration={.5}>
-                      <div id="profile-wrapper" className="profile-wrapper">
-                        <img src={Profile} alt="" className="img-responsive img-profile" />
-                        <IconContainer icon='user'>
-                          Amelia Carlie
-                        </IconContainer>
-                        <IconContainer icon='at'>
-                          ameliacarlie@gmail.com
-                        </IconContainer>
-                        <IconContainer icon='map-marker-alt'>
-                          Chapel Hill, NC
-                        </IconContainer>
-                      </div>
-                    </ScrollAnimation>
+                  <div className="col col-3">
+                    hello there
                   </div>
 
-                  <div className="col col-4">
-                    <IconContainer icon='info' info fade>
-                      <p>
-                        I am a front-end developer passionate about UI/UX. With 5 years experience in the graphic design field, I am now applying my knowledge of color theory and composition to my daily development projects. In 2019, I earned a certificate in Full-Stack Web Development from The Coding Bootcamp @ UNC.
-                      </p>
-                    </IconContainer>
-                    <IconContainer icon='code' info fade>
-                      <ul className="primary-list">
-                        <li><span className="heading">Languages:</span> HTML5, CSS3, JavaScript(ES5 & ES6), TypeScript</li>
-                        <li><span className="heading">Frameworks:</span> React, Angular</li>
-                        <li><span className="heading">Libraries:</span> jQuery, Materialize, Bootstrap</li>
-                        <li><span className="heading">Pre-Processors:</span> SASS</li>
-                        <li><span className="heading">Back-End:</span> Express, MySQL, MongoDB, Mongoose, Firebase</li>
-                        <li><span className="heading">Other Technologies:</span> Node.js</li>
-                      </ul>
-                    </IconContainer>
-                    <IconContainer icon='pencil-ruler' info fade>
-                      <ul className="primary-list">
-                        <li><span className="heading">Tools:</span> PhotoShop, Illustrator, InDesign, Adobe XD, Figma</li>
-                        <li><span className="heading">Design:</span> Color Theory, Illustration, Typography, Branding, Composition</li>
-                      </ul>
-                    </IconContainer>
-                    <IconContainer icon='pizza-slice' info fade>
-                      <ul className="primary-list">
-                        <li><span className="heading">Hobbies:</span> Photography, Making Pizza</li>
-                      </ul>
-                    </IconContainer>
+                  <div className="col col-3">
+                    reallly
                   </div>
                 </div>
-              </div>
-
-              <div className="wrapper-nav">
-                <a href="#work" className="wrapper-link">Work</a>
-                <span className="fas fa-chevron-down"></span>
               </div>
 
             </section>
 
             {/* Work */}
-            <section className="container container-dark" id="work">
-
-              <div className="wrapper-nav">
-                <span className="fas fa-chevron-up"></span>
-                <a href="#about" className="wrapper-link">About</a>
-              </div>
+            <section className="g-container g-container--dark" id="work">
 
               <div className="wrapper">
                 <div className="row">
@@ -278,7 +182,7 @@ class App extends Component {
 
             <div className="text-center text-sm footer-bottom">
               <p> - Created with React <i className="fab fa-react"></i> - </p>
-              <p>© Amelia Carlie 2019</p>
+              <p>© Amelia Carlie 2021</p>
             </div>
 
           </footer>
