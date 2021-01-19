431aeb6525ff062ab86d65cf1e6a0d5045ccbc1a
acarlie <ameliacarlie@gmail.com>
2021-01-19 12:46:38 -0500

    refactor(scss): modularized scss, updated files

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,39 +1,43 @@
 import React from 'react';
-import portfolio from './Portfolio.module.scss';
-import portfolioItem from './PortfolioItem.module.scss';
 import Link from './../Link';
 
+/**
+ * Styles
+ */
+import styles from './Portfolio.module.scss';
+import heading from './../../scss/modules/Heading.module.scss';
+import linkList from './../../scss/modules/LinkList.module.scss';
+import text from './../../scss/modules/Text.module.scss';
+import utility from './../../scss/modules/Utility.module.scss';
+
 function PortfolioItem(props) {
-    const direction = props.odd ? portfolioItem.odd : portfolioItem.even;
+    const direction = props.odd ? styles.odd: styles.even;
     const isCodepen = props.url.indexOf("codepen") > -1;
 
     return (
-        <li className={portfolioItem.item}>
-            <div className={portfolioItem.inner + ' ' + direction}>
-
-                <div className={portfolioItem.img_cont}>
-                    <img className={portfolioItem.img} src={process.env.PUBLIC_URL + '/assets/images/sm/' + props.img} alt={'Screen capture of ' + props.title} />
+        <li className={styles.item}>
+            <div className={styles.inner + ' ' + direction}>
+                <div className={styles.wrapper}>
+                    <img className={styles.img} src={process.env.PUBLIC_URL + '/assets/images/sm/' + props.img} alt={'Screen capture of ' + props.title} />
                 </div>
-                <div className={portfolioItem.info}>
+                <div className={styles.info + ' ' + text.small}>
                     {props.even}
-                    <h3 className="heading__6 heading__uppercase_sm">{props.title}</h3>
-                    <div className="text__small">{props.desc}</div>
-                    <ul className="list__links text__small" aria-label="Related links">
-                        {props.url && !isCodepen && <li className="list_item__link"><Link href={props.url} blank>View the deployment <span className="sr_only">of {props.title}</span>  ⟶ </Link></li>}
-                        {props.url && isCodepen && <li className="list_item__link"><Link href={props.url} blank>View <span className="sr_only">{props.title}</span> on CodePen ⟶  </Link></li>}
-                        {props.repo && <li className="list_item__link"><Link href={props.repo} blank>View the repo <span className="sr_only">for {props.title}</span> ⟶  </Link></li>}
+                    <h3 className={heading.small}>{props.title}</h3>
+                    <p>{props.desc}</p>
+                    <ul className={linkList.list} aria-label="Related links">
+                        {props.url && !isCodepen && <li className={linkList.item}><Link href={props.url} blank>View the deployment <span className={utility.sr_only}>of {props.title}</span>  ⟶ </Link></li>}
+                        {props.url && isCodepen && <li className={linkList.item}><Link href={props.url} blank>View <span className={utility.sr_only}>{props.title}</span> on CodePen ⟶  </Link></li>}
+                        {props.repo && <li className={linkList.item}><Link href={props.repo} blank>View the repo <span className={utility.sr_only}>for {props.title}</span> ⟶  </Link></li>}
                     </ul>
                 </div>
             </div>
-
         </li>
     )
 }
 
 function Portfolio(props) {
     return (
-        <div className={portfolio.wrapper}>
-
+        <div className={styles.container}>
             <ul id="portfolio" aria-label="Portfolio items">
                 {props.items &&
                     props.items.map((item, i) => {
--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import link from './SocialLink.module.scss';
-import socialLinks from './SocialLinks.module.scss';
+import styles from './SocialLinks.module.scss';
 
 function Link(props) {
     return (
-        <a className={link.link} href={props.url} target='_blank' rel='noopener noreferrer' aria-label={props.ariaLabel}>
-            <span className={'fab fa-' + props.icon + ' ' + link.span}></span>
+        <a className={styles.link} href={props.url} target='_blank' rel='noopener noreferrer' aria-label={props.ariaLabel}>
+            <span className={'fab fa-' + props.icon + ' ' + styles.span}></span>
         </a>
     );
 }
 
 function SocialLinks() {
     return (
-        <ul className={socialLinks.list} aria-label="Links to Amelia Carlie's social profiles">
-            <li className={socialLinks.item}><Link url='https://www.linkedin.com/in/acarlie' icon='linkedin-in' ariaLabel="View Linked In profile for Amelia Carlie, opens in new tab" /></li>
-            <li className={socialLinks.item}><Link url='https://github.com/acarlie' icon='github' ariaLabel="View GitHub profile for Amelia Carlie, opens in new tab" /></li>
-            <li className={socialLinks.item}><Link url='https://codepen.io/acarlie/' icon='codepen' ariaLabel="View CodePen profile for Amelia Carlie, opens in new tab" /></li>
+        <ul className={styles.list} aria-label="Links to Amelia Carlie's social profiles">
+            <li className={styles.item}><Link url='https://www.linkedin.com/in/acarlie' icon='linkedin-in' ariaLabel="View Linked In profile for Amelia Carlie, opens in new tab" /></li>
+            <li className={styles.item}><Link url='https://github.com/acarlie' icon='github' ariaLabel="View GitHub profile for Amelia Carlie, opens in new tab" /></li>
+            <li className={styles.item}><Link url='https://codepen.io/acarlie/' icon='codepen' ariaLabel="View CodePen profile for Amelia Carlie, opens in new tab" /></li>
         </ul>
     );
 }
